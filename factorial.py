756c99b1c411f41d9e02a771efb51e0dfa48788f
Ismaila Bah <Ismaila.bah@stu.ocsb.ca>
2023-11-21 03:35:50 +0000

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,28 +1,32 @@
-def calculate_factorial():
-    while True:
-        try:
-            # Get a whole number from the user
-            user_input = int(input("Enter a whole number: "))
-
-            if user_input < 0:
-                print("Please enter a non-negative whole number.")
-                continue  # Restart the loop if a negative number is entered
-            else:
-                break  # Exit the loop if a valid whole number is provided
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-
-    # Calculate the factorial using a do..while loop
-    factorial_result = 1
-    current_number = 1
-
-    while current_number <= user_input:
-        factorial_result *= current_number
-        current_number += 1
-
-    # Display the result
-    print(f"The factorial of {user_input} is: {factorial_result}")
+import random
 
 
-# Call the function to run the program
-calculate_factorial()
+def is_numeric(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+# Generate a random correct guess between 1 and 20
+correct_guess = random.randint(1, 20)
+
+while True:
+    # Ask the user to guess the number
+    user_guess = input("Guess the number between 1 and 20: ")
+
+    # Check if the input is a valid number
+    if not is_numeric(user_guess):
+        print("Invalid input. Please enter a valid number.")
+        continue  # Go back to the start of the loop
+
+    # Convert the user's guess to an integer
+    user_guess = int(user_guess)
+
+    # Check if the user's guess is correct
+    if user_guess == correct_guess:
+        print("Congratulations! You guessed the correct number.")
+        break  # Exit the loop because the guess is correct
+    else:
+        print("Sorry, that's not the right number. Try again.")
